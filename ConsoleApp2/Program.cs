bad4cfb334d15b6014dfb8ec7d4ad36faffdf50d
YaroslavKolchin <39592718+YaroslavKolchin@users.noreply.github.com>
2019-03-06 22:58:05 +0600

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyboardDat
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //zadan();
+            zadan2();
+        }
+
+        static void zadan()
+        {
+            Console.WriteLine("Введите число: ");
+            int num = int.Parse(Console.ReadLine());
+            int[] arr = new int[num.ToString().Length];
+            int i = 0, s = 0;
+            while (num > 0)
+            {
+                arr[i] = num % 10;
+                num /= 10;
+                s += arr[i];
+                i++;
+            }
+            Console.WriteLine("Сумма цифр " + s);
+            Console.ReadLine();
+        }
+
+        static void zadan2()
+        {
+            Console.WriteLine("Введите ФИО");
+            string name =(Console.ReadLine());
+            string surname = (Console.ReadLine());
+            string fname = (Console.ReadLine());
+            Console.WriteLine("здраствуй "+name+" "+surname+" "+fname);
+            Console.ReadLine();
+        }
+    }
+}
